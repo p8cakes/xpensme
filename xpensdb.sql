3aeb58e04d80a2bfaa66c07ea82d648f57c441d6
Sundar Krishnamurthy <sundar@passion8cakes.com>
2020-09-26 16:39:14 -0700

    Update xpensdb.sql

--- xpensdb.sql
+++ xpensdb.sql
@@ -6,17 +6,21 @@
 --
 -- xm.sql file to create the database, load metadata tables and stored procedures - please employ MySQL 5.5 or greater
 --
--- 1.    T1:    systemSettings table - table to store system settings, queried at session start and other places.
--- 2.    P1:    populateSystemSettings SP - to get initial set of settings required for app.
--- 3.    P2:    getSystemSetting SP - to get the value for a provided setting name.
+-- 1.    T1. appLogs table to store application logs
+-- 2.    P1. addAppLog stored procedure to add a certain string or log to the appLogs table
+-- 3.    P2. showLatestAppLogs stored procedure to fetch the latest configurable amount of logs inserted
+-- 4.    P3. clearAppLogs stored procedure to remove all entries from appLogs table
+-- 5.    T2.    systemSettings table - table to store system settings, queried at session start and other places.
+-- 6.    P4:    populateSystemSettings SP - to get initial set of settings required for app.
+-- 7.    P5:    getSystemSetting SP - to get the value for a provided setting name.
 --
 -- Revisions:
 --      1. Sundar Krishnamurthy         sundar@passion8cakes.com               9/25/2020       Initial file created.
 
 -- Very, very, very bad things happen if you uncomment this line below. Do at your peril, you have been warned!
-drop database if exists $$DATABASE_NAME$$;                                                        -- $$ DATABASE_NAME $$
+-- drop database if exists $$DATABASE_NAME$$;                                                        -- $$ DATABASE_NAME $$
 
--- Create database xpensDB, with utf8 and utf8_general_ci
+-- Create database $$DATABASE_NAME$$, with utf8 and utf8_general_ci
 create database if not exists $$DATABASE_NAME$$ character set utf8 collate utf8_general_ci;       -- $$ DATABASE_NAME $$
 
 -- Employ $$DATABASE_NAME$$
@@ -36,6 +40,7 @@ drop procedure if exists addAppLog;
 
 delimiter //
 
+-- 2. P1. addAppLog stored procedure to add a certain string or log to the appLogs table
 create procedure addAppLog(
     in            p_log                       varchar( 255 )
 )
@@ -53,10 +58,11 @@ end //
 
 delimiter ;
 
-drop procedure showLatestAppLogs;
+drop procedure if exists showLatestAppLogs;
 
 delimiter //
 
+-- 3. P2. showLatestAppLogs stored procedure to fetch the latest configurable amount of logs inserted
 create procedure showLatestAppLogs(
     in            p_count                     int ( 10 ) unsigned
 )
@@ -70,9 +76,22 @@ end //
 
 delimiter ;
 
+drop procedure if exists clearAppLogs;
+
+delimiter //
+
+-- 4. P3. clearAppLogs stored procedure to remove all entries from appLogs table
+create procedure clearAppLogs()
+begin
+    truncate table appLogs;
+
+end //
+
+delimiter ;
+
 -- drop table if exists systemSettings;
 
--- 1. T1. systemSettings table to store system settings
+-- 5. T2. systemSettings table to store system settings
 create table if not exists systemSettings (
     id                                        int ( 10 ) unsigned              not null auto_increment,
     name                                      varchar( 32 )                    not null,
@@ -81,14 +100,14 @@ create table if not exists systemSettings (
     created                                   datetime                         not null,
     lastUpdate                                datetime                         not null,
     key ( id ),
-    index ix_name ( name )
+    unique index ix_name ( name )
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
 
 drop procedure if exists populateSystemSettings;
 
 delimiter //
 
--- 2. P1. populateSystemSettings SP to get initial set of settings required for app.
+-- 6. P1. populateSystemSettings SP to get initial set of settings required for app.
 create procedure populateSystemSettings()
 begin
 
@@ -112,7 +131,7 @@ drop procedure if exists getSystemSetting;
 
 delimiter //
 
--- 3. P2. getSystemSetting SP to get the value for a provided setting name.
+-- 7. P5. getSystemSetting SP to get the value for a provided setting name.
 create procedure getSystemSetting(
     in            p_name                      varchar( 32 )
 )
